c42a87ee5e031299c344832cf2cb79d35ee15f8d
Jefferson Sousa de Paiva <x362624@rede.sp>
2024-11-27 15:38:44 -0300

    usando eventos com JS

--- scripts/calculadora.js
+++
@@ -1,3 +0,0 @@
-let calculadora = new CalcController;
-
-
--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,24 +1,123 @@
 class CalcController {
     //o comando this = referencia atributos e metodos
     constructor() {
-        this._displayCalc = "0";
-        this._dataAtual
+        this._operation = [];
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector("#display")
+        this._dateEl = document.querySelector("#data")
+        this._timeEl = document.querySelector("#hora")
+        this._currentDate
+
+        this.initialize();
+        this.initButtonsEvents()
     }
 
+    initialize() {
+        this.setDisplayDateTime()
+        // Função executada em um intervalo de tempo
+        setInterval(() => {
+            this.setDisplayDateTime()
+        }, 1000)
+
+
+
+    }
+
+    addEventListenerAll(element, events, fn) {
+
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false)
+        })
+    }
+
+    clearAll(){
+
+    }
+
+    clearEntry(){
+
+    }
+    execBtn(value){
+        switch (value){
+            case 'ac':
+            this.clearAll();
+            break;
+
+            case 'ce':
+                this.clearEntry()
+            break;
+
+            case 'soma':
+                break;
+            case 'subtracao':
+                break;
+            case 'divisao':
+                break;
+            case 'multiplicacao':
+                break;
+            case 'soma':
+                break;
+            case 'soma':
+                break;
+            case 'soma':
+                break;
+        }
+    }
+    initButtonsEvents() {
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        // forEach para percorrer cada botao
+        buttons.forEach((btn, index) => {
+            
+            this.addEventListenerAll(btn, 'click drag', e => {
+
+                let textBtn = btn.className.baseVal.replace("btn-", "")
+
+                this.execBtn();
+            })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer"
+            })
+        })
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        })
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML
+    }
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value
+    }
+    get displayDate() {
+        return this._dateEl.innerHTML
+    }
+
+    set displayDate(value) {
+        return this._dateEl.innerHTML = value
+    }
     // os metodos getters e setters = permitem definir como acessar os valores
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor) {
-        this._displayCalc = valor;
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
     }
 
-    get dataAtual() {
-        return this._dataAtual
+    get currentDate() {
+        return new Date()
     }
 
-    set dataAtual(valor) {
-        this._dataAtual = valor
+    set currentDate(value) {
+        this._currentDate = value
     }
 }
