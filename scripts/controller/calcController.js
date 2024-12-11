e16f363ede9b62334c06aae397385fe448b7396b
Jefferson Sousa de Paiva <x362624@rede.sp>
2024-12-11 14:41:06 -0300

    atualizacao na calculadora

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -30,36 +30,98 @@ class CalcController {
         })
     }
 
-    clearAll(){
-
+    clearAll() {
+        this._operation = []
     }
 
-    clearEntry(){
-
+    clearEntry() {
+        this._operation.pop();
     }
-    execBtn(value){
-        switch (value){
+
+    getLastOperation(value) {
+        return this._operation[this._operation.length - 1] = value
+    }
+
+    setLastOperation(){
+        this._operation[this._operation.length - 1] = value
+    }
+
+    isOperator(value) {
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1)
+    }
+
+    addOperation(value) {
+
+        if (isNaN(this.getLastOperation())) {
+
+            if (this.isOperator(value)) {
+             this._setLastOperation(value)
+
+            } else if(isNaN) {
+
+            }else{
+                this._operation.push(value)
+            }
+
+        } else {
+            //Number
+            let newValue = this.getLastOperation().toString() + value.toString()
+            this.setLastOperation(parseInt(newValue))
+        }
+
+        this._operation.push(value)
+    }
+    setError() {
+        this.displayCalc = "Error"
+    }
+    execBtn(value) {
+        switch (value) {
             case 'ac':
-            this.clearAll();
-            break;
+                this.clearAll();
+                break;
 
             case 'ce':
                 this.clearEntry()
-            break;
+                break;
 
             case 'soma':
+                this.addOperation('+')
                 break;
             case 'subtracao':
+                this.addOperation('-')
                 break;
             case 'divisao':
+                this.addOperation('/')
                 break;
             case 'multiplicacao':
+                this.addOperation('*')
                 break;
-            case 'soma':
+            case 'porcento':
+                this.addOperation('%')
                 break;
-            case 'soma':
+            case 'igual':
                 break;
-            case 'soma':
+
+            case 'ponto':
+                this.addOperation('.')
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError()
                 break;
         }
     }
@@ -68,7 +130,7 @@ class CalcController {
 
         // forEach para percorrer cada botao
         buttons.forEach((btn, index) => {
-            
+
             this.addEventListenerAll(btn, 'click drag', e => {
 
                 let textBtn = btn.className.baseVal.replace("btn-", "")
